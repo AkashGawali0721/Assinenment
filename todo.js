a523c5cf3a5ed1d8a218b0e72fa1f989c0cfcfff
Akash Gawali <gawaliakash43@gmail.com>
2024-11-26 09:31:02 +0530

    second Question

---
+++ todo.js
@@ -0,0 +1,15 @@
+
+let list=[
+   
+  "walk 5 km",
+  "take bath",
+  "breakfast and coffee",
+  "going to office",
+  "take lunch "
+];
+
+list.unshift("Wake up at 7:00 AM");
+
+list.pop("take lunch")
+
+console.log(list);
