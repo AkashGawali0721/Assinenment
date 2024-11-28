3e70b930e4fcd4c4403d8d708f681940f8d7d91e
Akash Gawali <gawaliakash43@gmail.com>
2024-11-28 09:21:38 +0530

    second Question largest number

---
+++ largest.js
@@ -0,0 +1,16 @@
+let num1=40;
+let num2=78;
+let num3=33;
+
+if(num1 > num2 && num1 > num3){
+    console.log("num1 is largest number");
+}
+else if( num2 > num1 && num2 > num3){
+    console.log("num2 is largest number");
+}
+else if(num3 > num1 && num3 >num2){
+    console.log("num3 is largest number");
+}
+else{
+    console.log("Both numbers are same");
+}
