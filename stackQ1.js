78da4f6511bc9275c49cb928eff04223d9c844d7
Akash Gawali <gawaliakash43@gmail.com>
2024-12-04 10:37:30 +0530

    stack Q1

---
+++ stackQ1.js
@@ -0,0 +1,18 @@
+
+var age= 23;
+  
+  function displayage(){
+    
+    console.log(age);
+    
+  }
+  function changeage(age){
+   
+    
+    console.log(age);
+  }
+  
+ displayage(); 
+  changeage(24);
+  
+  
